b6a7f5b21d7d8c1a2cfbecb408ac75c442450840
digitalh2o <digitalh2o@gmail.com>
2018-05-02 12:22:15 -0500

    added skeleton css and initial styling before displaying api data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 import SidebarTopics from "./components/SidebarTopics";
+import "./css/normalize.css";
+import "./css/skeleton.css";
+import './css/custom.css'
 import api from "./utils/api";
-import "./App.css";
 
 class App extends Component {
   constructor(props) {
@@ -17,7 +19,9 @@ class App extends Component {
       .hackerHuntPromise()
       .then(res => res.json())
       .then(res => {
-        console.log(res);
+        this.setState({
+          news: res
+        });
       })
       .catch(err => {
         console.log(err);
@@ -25,10 +29,22 @@ class App extends Component {
   }
 
   render() {
+    const { news } = this.state;
+    console.log("the news", news);
     return (
-      <div className="App">
-        <SidebarTopics />
-        <h1>Hello</h1>
+      <div className="container">
+        <div className="row">
+          <div className="two columns">
+            <SidebarTopics />
+          </div>
+          <div className="ten columns">
+          
+          <input />
+          
+            
+            <h1>Hello</h1>
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/SidebarTopics.js
+++ src/components/SidebarTopics.js
@@ -1,11 +1,40 @@
 import React from "react";
 
+const topics = [
+  { route: "development", title: "Development" },
+  { route: "system", title: "System" },
+  { route: "tools", title: "Tools" },
+  { route: "data-science", title: "Data science" },
+  { route: "blockchain", title: "Blockchain" },
+  { route: "mobile", title: "Mobile" },
+  { route: "awesome-lists", title: "Awesome lists" },
+  { route: "social", title: "Social" },
+  { route: "visual", title: "Visual" },
+  { route: "open-source", title: "Open source" },
+  { route: "all-topics", title: "All topics" }
+];
+
 const SidebarTopics = props => {
   return (
-    <section>
+    <section className="sidebar">
+      <svg
+        width="32"
+        height="32"
+        viewBox="0 0 32 32"
+      >
+        <path
+          d="M27 17v7h5v-24h-5v12h-4v-12h-5v24h5v-7h4zm-27 11h32v4h-32v-4zm0-28h5v24h-5v-24zm9 0h5v24h-5v-24zm-4 12h4v5h-4v-5z"
+          fill="#f60"
+        />
+      </svg>
       <h5>Topics</h5>
-
-      {/* to fill out */}
+      {topics.map(topic => {
+        return (
+          <div className="topic_container">
+            <a href={`topic/${topic.route}/trending`}>{topic.title}</a>
+          </div>
+        );
+      })}
     </section>
   );
 };
